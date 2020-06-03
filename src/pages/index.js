154014c37f249ab860954f7b335b737e0381991e
Pascal van der Horst <Pascal.van.der.Horst@avivasolutions.nl>
2020-06-03 19:49:48 +0200

    Another commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,7 @@ const IndexPage = () => (
     <p>Now go build something great.</p>
     <p>Now go build something great.</p>
     <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
