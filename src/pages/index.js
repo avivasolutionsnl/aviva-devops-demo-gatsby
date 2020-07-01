6c17862387901fd13a2d18ae173c6e0d552ef379
Pascal van der Horst <Pascal.van.der.Horst@avivasolutions.nl>
2020-07-01 15:04:12 +0200

    Removed pars from master

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,12 +10,6 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Welcome to your new Gatsby site.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
